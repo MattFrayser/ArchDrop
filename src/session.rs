9340a7ba2132b4d49ff70af813aa14f5ff0a804b
mattfrayser <mattfrayser@gmail.com>
2025-11-21 16:01:46 -0500

    Created sessions. Create session makes a one time use token, validates token exits and uses it.

---
+++ src/session.rs
@@ -0,0 +1,59 @@
+use uuid::Uuid;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::collections::HashMap;
+
+#[derive(Clone)]
+pub struct SessionStore {
+    sessions: Arc<Mutex<HashMap<String, SessionData>>>,
+}
+
+pub struct SessionData {
+    pub file_path: String, 
+    pub used: bool, // flag to prevent replay attacks
+}
+
+impl SessionStore {
+    pub fn new() -> Self {
+        Self { 
+            // wrap hashmap in mutex for safe access
+            sessions: Arc::new(Mutex::new(HashMap::new())), 
+        }
+    }
+
+    pub async fn create_session(&self, file_path: String) -> String {
+
+        let token = Uuid::new_v4().to_string();
+
+        // Acquire lock to HashMap
+        // if annother tasks holds lock, await (doesnt block thread)
+        let mut sessions = self.sessions.lock().await;
+
+        // clone() is used since HashMap::insert takes ownership of the key
+        // without it token would move and be unavailable for return 
+        sessions.insert(token.clone(), SessionData {
+            file_path, 
+            used: false,
+        });
+
+        token // return ownership of token to caller 
+    }
+
+    pub async fn validate_and_mark_used(&self, token: &str) -> Option<String> {
+
+        let mut sessions = self.sessions.lock().await;
+
+        if let Some(session) = sessions.get_mut(token) {
+            if !session.used {
+                // mark as used FIRST, prevent possible race condition
+                session.used = true;
+
+                // Hashmap owns String so clone it to return ownership
+                return Some(session.file_path.clone());
+            }
+        }
+
+        // Token doesnt exists or is already used
+        None
+    }
+}
